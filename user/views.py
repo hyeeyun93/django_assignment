cad7651f8a40be19e9e1a6e1fabde16ec2cb3468
hyeeyun93 <tkdcjxntjddl93@gmail.com>
2025-01-22 21:21:44 +0900

    Edited user app

--- user/views.py
+++ user/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+from django.contrib.auth.views import LoginView, LogoutView
+from .forms import CustomUserCreationForm
 
 # Create your views here.
+
+# Signup View
+class SignUpView(CreateView):
+    form_class = CustomUserCreationForm
+    template_name = 'user/signup.html'
+    success_url = reverse_lazy('login')
+    
+# Login view
+class CustomLoginView(LoginView):
+    template_name = 'user/login.html'
+    
+# Logout view
+class CustomLogoutView(LogoutView):
+    template_name = 'user/logout.html'
